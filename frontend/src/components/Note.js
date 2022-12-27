b936fc13dbc226ebd64d4c786f2c7aa98cbe8f69
Nicolas Schleicher <nicosc41@gmail.comm>
2022-12-27 20:58:23 -0300

    <add> add basic components

---
+++ frontend/src/components/Note.js
@@ -0,0 +1,18 @@
+import React  from 'react'
+
+
+const Note = (props) =>{
+
+    return(
+        <div id = "Note">
+            <tr>
+                <th>imagen</th>
+                <th>inforacion</th>
+                <th>opcion eliminar</th>
+                <ht>opcion editar</ht>
+            </tr>
+        </div>
+    )
+}
+
+export default Note
